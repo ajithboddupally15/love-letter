c2f8187eaf19ab8c463bc90f267031bcf9f1a634
ajithboddupally15 <167822265+ajithboddupally15@users.noreply.github.com>
2024-04-22 18:27:08 -0400

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -35,10 +35,10 @@ const LoveLetter = () => {
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        mah dear kundanapu bomma🖤,<br />
-        never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
+        my dear bujju🖤,<br />
+        never have i been so blessed as to fall in love with someone as wonderful as you..thanks for being with me.<br />i loveeee youuuu :)<br />
         always yours,<br />
-        @niihaaarrrr
+        @AJ
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
